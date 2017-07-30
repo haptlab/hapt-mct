619f68700c2e86f222208c1da7f67b2f05dc9779
Hideyuki Takei <takehide22@gmail.com>
2017-07-30 10:50:08 +0900

    First commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hapt-mct'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
