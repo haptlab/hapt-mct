01be3a0af8f9af5ab2099c8ace4b5c7c555b7c8c
Hideyuki Takei <takehide22@gmail.com>
2017-08-01 05:35:49 +0900

    implement video transfering app

---
+++ protobuf/test_server.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import cv2
+from fpv_image_pb2 import *
+
+import string,cgi,time
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+WIDTH = 80 #100
+HEIGHT = 60 #75
+
+cap = cv2.VideoCapture(0)
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, (WIDTH, HEIGHT))
+            #frame = cv2.resize(frame, (400, 300))
+
+            # mirror
+            frame = frame[:,::-1]
+
+            cv2.imwrite("image.jpg", frame)
+
+	    fimage = FpvImage()
+	    fimage.width = WIDTH
+	    fimage.height = HEIGHT
+	    fimage.image = open("image.jpg").read()
+	    data = fimage.SerializeToString()
+
+            self.send_response(200)
+            self.send_header("Content-type", "binary/octet-stream")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            self.wfile.write(data)
+            return
+
+        except IOError:
+            self.send_error(404, "File Not Found: %s" % self.path)
+
+if __name__ == "__main__":
+    try:
+        server = HTTPServer(('', 9999), MyHandler)
+        print "started httpserver..."
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "^C received, shutting down server"
+        cap.release()
+        server.socket.close()
+
+    #while True:
+    #    ret, frame = cap.read()
+    #    frame = cv2.resize(frame, (133, 100))
+    #    frame = cv2.resize(frame, (400, 300))
+
+    #    # mirror
+    #    frame = frame[:,::-1]
+
+    #    # show
+    #    cv2.imshow("frame", frame)
+    #    #cv2.imwrite("image.jpg", frame)
+
+    #    k = cv2.waitKey(1)
+    #    if k == 27: # press esc
+    #        break
+
+    #cap.release()
+    #cv2.destroyAllWindows()
+
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,23 +1,102 @@
 <template>
   <div id="wrapper">
-    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+    <button @click="onClick"
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
       Button
     </button>
+    <img class="fpv-image" :src="image">
     <!--img id="logo" style="width: 100px" src="~@/assets/logo.png" alt="electron-vue"-->
   </div>
 </template>
 
 <script>
+  import protobuf from 'protobufjs'
+
+  let FpvImage
+
   export default {
-    components: {},
+    data () {
+      return {
+        image: null
+      }
+    },
+
     methods: {
+      concatTypedArrays (a, b) { // a, b TypedArray of same type
+        let c = new (a.constructor)(a.length + b.length)
+        c.set(a, 0)
+        c.set(b, a.length)
+        return c
+      },
+
+      pump (reader, chunks, callback) {
+        return reader.read().then((result) => {
+          if (result.value) {
+            if (chunks) {
+              chunks = this.concatTypedArrays(chunks, result.value)
+            } else {
+              chunks = result.value
+            }
+            // chunks.push(result.value)
+          }
+
+          if (result.done) {
+            callback(chunks)
+          } else {
+            return this.pump(reader, chunks, callback)
+          }
+        }).catch((e) => {
+          console.log(e)
+        })
+      },
+
+      pumpPromise (reader) {
+        let chunks = null // []
+        return new Promise((resolve) => {
+          this.pump(reader, chunks, (chunks) => {
+            resolve(chunks)
+          })
+        })
+      },
+
+      onClick () {
+        fetch('http://localhost:9999')
+          .then((response) => {
+            return this.pumpPromise(response.body.getReader())
+          })
+          .then((binaryData) => {
+            let message = FpvImage.decode(binaryData)
+            let base64Data = btoa(String.fromCharCode.apply(null, message.image))
+            // console.log(message, base64Data)
+            this.image = 'data:image/png;base64,' + base64Data
+          })
+      },
+
       open (link) {
         this.$electron.shell.openExternal(link)
       }
+    },
+    created () {
+      protobuf.load('static/fpv_image.proto', (err, root) => {
+        if (err) {
+          console.log(err)
+        }
+
+        FpvImage = root.lookupType('hapt.FpvImage')
+
+//        setInterval(() => {
+//          this.onClick()
+//        }, 150)
+        // console.log(FpvImage)
+      })
     }
   }
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  .fpv-image {
+    width: 400px;
+    height: 300px;
+    background: #555;
+  }
 </style>
